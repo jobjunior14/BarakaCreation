cc6a7aaf1c6d5c73dcc0f335255c70e1b4651a5d
Job Junior <102473673+jobjunior14@users.noreply.github.com>
2024-01-31 18:50:41 +0200

    3rd part addes nos valeur, apropos

--- src/components/aPropos.jsx
+++ src/components/aPropos.jsx
@@ -11,6 +11,9 @@ import certifié_logo_4 from '../assets/A propos/certifié_logo_4.png';
 import certifié_Logo_5 from '../assets/A propos/certifié_Logo_5.png';
 import certifié_Logo_6 from '../assets/A propos/certifié_Logo_6.png';
 import certifié_Logo_7 from '../assets/A propos/certifié_Logo_7.png';
+import icone_innovation from '../assets/A propos/icone_innovation.png';
+import icone_passion from '../assets/A propos/icone_passion.png';
+import icone_discipline from '../assets/A propos/icone_discipline.png';
 export default function Apropos () {
 
      const [index, setIndex] = useState(0);
@@ -33,7 +36,7 @@ export default function Apropos () {
             <div className=" w-full flex flex-col sm:flex-row justify-center text-center md:pt-5">
                 
                 {/* pc's picture  */}
-                <div className=' hidden md:flex justify-center place-items-end px-6 bg-bgFormation  md:w-[40%]  '>
+                <div className=' hidden md:flex justify-center place-items-end px-6 bg-b  '>
                     <img src={moii_2} alt='moi' className='h-auto'/>
                 </div>
 
@@ -120,6 +123,54 @@ export default function Apropos () {
                     <div className={` lg:min-w-32 lg:h-20 min-w-20 h-20 flex items-center ease-out delay-100 hover:transform-none hover:cursor-pointer  duration-100`} style={{transform: `translate3d(${-timers}px, 0, 0)`}}> <img src={certifié_Logo_6} alt='logo' /></div> 
                     <div className={` lg:min-w-32 lg:h-20 min-w-20 h-20 flex items-center ease-out delay-100 hover:transform-none hover:cursor-pointer  duration-100`} style={{transform: `translate3d(${-timers}px, 0, 0)`}}> <img src={certifié_Logo_7} alt='logo' /></div> 
                 </div>
+            </div>
+
+            {/* nos valeurs */}
+            <div className=' bg-bgFormation w-full p-5 sm:p-7 md:p-9 lg:p-14 flex flex-col gap-y-5 md:items-center md:justify-center'>
+
+                <div className='flex flex-col gap-y-5 md:max-w-[60%] '>
+                    <h1 className='text-5xl sm:text-6xl md:text-7xl text-left text-nos font-bold '>Nos <span className='text-nosVal'>valeurs</span></h1>
+
+                    <div className='flex flex-col gap-3 md:items-center md:justify-center'>
+                        
+                        {/* innovation  */}
+                        <div className='text-left flex gap-3 md:gap-9 justify-normal w-full '>
+                            <div className='w-[12%]  pt-1 md:pt-0 flex md:items-center'>
+                                <img src={icone_innovation} className='h-10 md:h-auto' alt='icone' />
+                            </div>
+
+                            <div className='w-[80%] flex flex-col md:justify-center '>
+                                <h2 className='text-gray-700 text-xl sm:text-2xl md:text-3xl font-bold'>Innvation</h2>
+                                <p className='text-gray-800 text-[10px] sm:text-[12px] md:text-[14px]'>  Nous nous engageons à apporter de la valeur et vous faire vivre une expérience mémorable. Chaque nouveau projet est pour nous un défi à relever; une occasion de repousser nos limites et celles de nos clients. <br/> C&apos;est ce qui nous a permis de satisfaire plus de 90 % de +250 projets réalisés</p>
+                            </div>
+                        </div>
+
+                        {/* passion  */}
+                        <div className='text-left flex gap-3 md:gap-9 justify-normal w-full '>
+                            <div className='w-[12%]  pt-1 md:pt-0 flex md:items-center'>
+                                <img src={icone_passion} className='h-10 md:h-auto' alt='icone' />
+                            </div>
+
+                            <div className='w-[80%] flex flex-col md:justify-center '>
+                                <h2 className='text-gray-700 text-xl sm:text-2xl md:text-3xl font-bold'>Passion</h2>
+                                <p className='text-gray-800 text-[10px] sm:text-[12px] md:text-[14px]'>  Nous sommes animés par la passion de créer des œuvres d&apos;art à la fois belles et significatives. Elle nous permet de partager nos idées avec le monde entier et nous pousse donner une grande considération à chaque projet et chaque client</p>
+                            </div>
+                        </div>
+
+                        {/* discipline  */}
+                        <div className='text-left flex gap-3 md:gap-9 justify-normal w-full '>
+                            <div className='w-[12%]  pt-1 md:pt-0 flex md:items-center'>
+                                <img src={icone_discipline} className='h-10 md:h-auto' alt='icone' />
+                            </div>
+
+                            <div className='w-[80%] flex flex-col md:justify-center '>
+                                <h2 className='text-gray-700 text-xl sm:text-2xl md:text-3xl font-bold'>Discipline</h2>
+                                <p className='text-gray-800 text-[10px] sm:text-[12px] md:text-[14px]'>  Nous nous engageons à comprendre les besoins de nos clients, à leur proposer des solutions qui répondent à leurs attentes et à respecter les délais fixés. Nous travaillons de manière organisée et nous sécurisons les projets de nos clients pendant et après le contrat</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
