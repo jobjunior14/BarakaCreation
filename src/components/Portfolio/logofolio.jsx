c58483119fa54f3587078ff129f692fbac10d957
Job Junior <102473673+jobjunior14@users.noreply.github.com>
2024-02-21 11:33:42 +0200

    portfolio > logofolio missing details

--- src/components/Portfolio/logofolio.jsx
+++ src/components/Portfolio/logofolio.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import Accueil_Logofolio from '../../assets/Portofolio/Logofolio/Accueil_Logofolio.jpg';
 import Logo_AED_1 from '../../assets/Portofolio/Logofolio/Logo_AED_1.jpg';
 import Logo_Afrique_millenaire_3_ from '../../assets/Portofolio/Logofolio/Logo_Afrique_millenaire_3_.jpg';
@@ -6,6 +7,8 @@ import Logo_Architectonic_2 from '../../assets/Portofolio/Logofolio/Logo_Archite
 import Logo_Zali_1 from '../../assets/Portofolio/Logofolio/Logo_Zali_1.jpg';
 import Logo_Transformation_church_1 from '../../assets/Portofolio/Logofolio/Logo_Transformation_church_1.jpg';
 import Logo_Odej_1 from '../../assets/Portofolio/Logofolio/Logo_Odej_1.jpg';
+import Logo_Odej_2 from '../../assets/Portofolio/Logofolio/Logo_Odej_2.jpg';
+import Logo_Odej_3 from '../../assets/Portofolio/Logofolio/Logo_Odej_3.jpg';
 import Logo_sos_libota_1 from '../../assets/Portofolio/Logofolio/Logo_sos_libota_1.jpg';
 import Logo_Smart_touch_3 from '../../assets/Portofolio/Logofolio/Logo_Smart_touch_3.jpg';
 import Logo_uibec_1 from '../../assets/Portofolio/Logofolio/Logo_uibec_1.jpg';
@@ -37,15 +40,145 @@ import Logo_Fojed_1 from '../../assets/Portofolio/Logofolio/Logo_Fojed_1.jpg';
 import Logo_Ecobuilding_1 from '../../assets/Portofolio/Logofolio/Logo_Ecobuilding_1.jpg';
 import Logo_centenaire_cepac_1 from '../../assets/Portofolio/Logofolio/Logo_centenaire_cepac_1.jpg';
 import Logo_Afrique_millenaire_1 from '../../assets/Portofolio/Logofolio/Logo_Afrique_millenaire_1.jpg';
+import {OnTopBtn} from '../services/identite_visuel/comp/util';
+import { useState, useEffect } from 'react';
 
-import { useState } from 'react';
+function LogoDetail ({images,text, title, state, hideDetails }) {
+    
+    const [index, setIndex] = useState(0);
 
-// function LogoDetail ({welcomImge, })
+      // slide the images automaticaly
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            
+            setIndex ( prev => prev === 200 ? 0 : prev + 100);
+        }, 5000) ;
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [index]);
+
+    const nextImage = () =>  {
+        setIndex ( prev => prev === 200 ? 0 : prev + 100);
+    };
+
+    const prevImage = () =>  {
+        setIndex ( prev => prev === 0 ? 200 : prev - 100);
+    };
+
+
+    return (<div className={`${ state ? 'absolute' : 'hidden'} p-5 sm:p-7 md:px-10 lg:px-[8%] w-full flex flex-col justify-center items-center gap-5 bg-slate-200 relative rounded-xl`}>
+            
+
+        {/* button to hide the details  */}
+        <div onClick={() => hideDetails()} className={`p-1 border border-gray-700 rounded-full cursor-pointer`}>
+           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+            </svg>
+        </div>
+
+        {/* images slider */}
+        {state && <section className='w-full flex justify-center overflow-hidden items-center gap-2'>
+            {/* style={{transform: `translateX(${-index}%)`}} */}
+            <div className='w-full flex duration-200' style={{transform: `translateX(${-index}%)`}} >
+                <img src={images[0]} alt='image'/>
+                <img src={images[1]} alt='image'/>
+                <img src={images[2]} alt='image'/>
+
+            </div>
+
+        </section>}
+
+        {/* circle div to show the image's number  */}
+        <secton className=' flex items-center justify-center w-full px-5 gap-2'>
+
+            <div onClick={() => setIndex(0)} className={`w-[10px] h-[10px] rounded-full border border-blue-200 transition-[background-color]  flex hover:bg-gray-400 ${index === 0 ? 'bg-gray-400' : ''} duration-300 delay-200 hover:cursor-pointer`}> </div>
+            <div onClick={() => setIndex(100)} className={`w-[10px] h-[10px] rounded-full border border-blue-200 transition-[background-color]  flex hover:bg-gray-400 ${index === 100 ? 'bg-gray-400' : ''} duration-300 delay-200 hover:cursor-pointer`}> </div>
+            <div onClick={() => setIndex(200)} className={`w-[10px] h-[10px] rounded-full border border-blue-200 transition-[background-color]  flex hover:bg-gray-400 ${index === 200 ? 'bg-gray-400' : ''} duration-300 delay-200 hover:cursor-pointer`}> </div>
+        </secton>
+
+        {/* section texte  */}
+        <section className='text-left flex flex-col gap-3 '>
+            <h1 className='font-bold text-[1rem] sm:text-[1.3rem] md:text-[1.5rem] text-gray-700'>{title}</h1>
+            <p className='text-[0.75rem] sm:text-[0.9rem] md:text-[1.1rem] md:leading-6 text-gray-700 w-[70%]'> {text}</p>
+        </section>
+
+        {/* div to prev or next images  */}
+        <div className=' flex items-center justify-evenly  w-full px-5'>
+            
+            <div className='md:w-[35px] md:h-[35px] sm:w-[1.875rem] sm:h-[1.875rem] w-[25px] h-[25px] items-center justify-center rounded-full sm:hover:bg-gray-300 duration-200 cursor-pointer pr-[4px] border border-gray-600 flex' onClick={prevImage}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-20 h-20 text-gray-600 sm:hover:text-gray-800 ">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                </svg>
+
+            </div>
+            
+            
+            <div className='md:w-[35px] md:h-[35px] sm:w-[1.875rem] sm:h-[1.875rem] w-[25px] h-[25px] items-center justify-center rounded-full sm:hover:bg-gray-300 duration-200 cursor-pointer pl-[4px] border border-gray-600 flex' onClick={nextImage}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-20 h-20 text-gray-600 sm:hover:text-gray-800 ">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+            </div>
+
+        </div>
+
+    </div>)
+}
 
 export default function Logofolio () {
+    
+    const [showDetails, setShowDetails] = useState({
+        a: false, b: false, c: false, d: false, e: false, f: false, g: false, h: false, i: false,
+        j: false, k: false, l: false, m: false, n: false, o: false, p: false, q: false,
+        r: false, s: false, t: false, u: false, v: false, w: false, x: false, y: false,
+        z: false, aa: false, bb: false, cc: false, dd: false, ee: false, ff: false, gg: false,
+        hh: false, ii: false, jj: false, kk: false, ll: false, mm: false,
+
+        hidden: function () {
+
+          return this.a || this.b || this.c || this.d || this.e || this.f || this.g || this.h || this.i || this.j || this.k || this.l || this.m || this.n || this.o || this.p || this.q || this.r || this.s || this.t || this.u || this.v || this.w || this.x || this.y || this.z || 
+          this.aa || this.bb || this.cc || this.dd || this.ee || this.ff || this.gg || this.hh || this.ii || this.jj || this.kk || this.ll || this.mm   
+        } 
+    });
+
+    const staticObj = {
+        a: false, b: false, c: false, d: false, e: false, f: false, g: false, h: false, i: false,
+        j: false, k: false, l: false, m: false, n: false, o: false, p: false, q: false,
+        r: false, s: false, t: false, u: false, v: false, w: false, x: false, y: false,
+        z: false, aa: false, bb: false, cc: false, dd: false, ee: false, ff: false, gg: false,
+        hh: false, ii: false, jj: false, kk: false, ll: false, mm: false,
+
+        hidden: function () {
+
+          return this.a || this.b || this.c || this.d || this.e || this.f || this.g || this.h || this.i || this.j || this.k || this.l || this.m || this.n || this.o || this.p || this.q || this.r || this.s || this.t || this.u || this.v || this.w || this.x || this.y || this.z || 
+          this.aa || this.bb || this.cc || this.dd || this.ee || this.ff || this.gg || this.hh || this.ii || this.jj || this.kk || this.ll || this.mm   
+        } 
+    }
+
+
+    const handleDetails = (letter) => {
+
+        setShowDetails({...staticObj, [letter]: true});
+    };
+
+    const hideDetails = () => {
+        setShowDetails({...staticObj});
+    }
 
     return (
-        <main className="w-full flex flex-col gap-5">
+        <main className="w-full flex flex-col gap-5 relative">
+            <div className={` ${showDetails.hidden() ? 'sticky' : 'hidden'} absolute top-5 z-40 backdrop-blur-md   w-full h-[100vh]  justify-center items-center flex px-[2%]`}>
+
+             <LogoDetail 
+                images={[Logo_Odej_2, Logo_Odej_3, Logo_Odej_1]}
+                title={'ODJ asbl'}
+                text={"Le coeur de l'identé visuelle est le Logo. Il est la premiere impression que les gens ont d'une entreprise, nous sommes spécialisés dans la conception des logos. Nous avons accompagnés l'organisation ODJ tout au long du processus du briefing à la création du logo officiel"}
+                hideDetails={hideDetails}
+                state={showDetails.f}
+            />                          
+            </div>
             <section className='w-full h-[18.75rem] sm:h-auto bg-cover sm:bg-none bg-fixed bg-center bg-no-repeat flex flex-col items-center justify-center' style={{backgroundImage: `url(${Accueil_Logofolio})`}}>
                         
                 <div className='h-full w-full duration-200 delay-150 relative'>        
@@ -55,6 +188,7 @@ export default function Logofolio () {
 
                     
             </section>
+            
 
             <section className="grid grid-cols-2 sm:grid-cols-3 w-full gap-2 px-[2%] bg-slate-300 py-5 sm:px-[10%]" >
 
@@ -78,7 +212,7 @@ export default function Logofolio () {
 
                 </div>
                 
-                <div className='bg-cover bg-center flex justify-center items-center w-full h-[10rem] sm:hover:scale-105 duration-150 sm:hover:-translate-x-5 sm:hover:-translate-y-2 rounded-lg ' style={{backgroundImage: `url(${Logo_Odej_1})`}}>
+                <div onClick={() => handleDetails('f')}  className='bg-cover cursor-pointer bg-center flex justify-center items-center w-full h-[10rem] sm:hover:scale-105 duration-150 sm:hover:-translate-x-5 sm:hover:-translate-y-2 rounded-lg ' style={{backgroundImage: `url(${Logo_Odej_1})`}}>
 
                 </div>                
                 
@@ -204,6 +338,8 @@ export default function Logofolio () {
                 </div>                
                               
             </section>
+
+            <OnTopBtn brdrColor='border-gray-700' textColor = ' text-black'/>
 
         </main>
     )
